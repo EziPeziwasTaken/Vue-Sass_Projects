73eb8dd6120a961f4ea3323fc42719b19520ebef
EziPeziwasTaken <cechstanda7@gmail.com>
2025-03-23 19:04:56 +0100

    Adds Calorie_Counter + To-do List

---
+++ Calorie_Counter/index.vue
@@ -0,0 +1,28 @@
+const { createApp } = Vue;
+
+        createApp({
+            data() {
+                return {
+                    newFood: '',
+                    newCalories: '',
+                    foods: []
+                };
+            },
+            computed: {
+                totalCalories() {
+                    return this.foods.reduce((sum, food) => sum + food.calories, 0);
+                }
+            },
+            methods: {
+                addFood() {
+                    if (this.newFood.trim() !== '' && this.newCalories > 0) {
+                        this.foods.push({ name: this.newFood, calories: this.newCalories });
+                        this.newFood = '';
+                        this.newCalories = '';
+                    }
+                },
+                removeFood(index) {
+                    this.foods.splice(index, 1);
+                }
+            }
+        }).mount('#app');
---
+++ To-do_List/index.vue
@@ -0,0 +1,24 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            newTask: '',
+            tasks: []
+        };
+    },
+    methods: {
+        addTask() {
+            if (this.newTask.trim() !== '') {
+                this.tasks.push({ text: this.newTask, completed: false });
+                this.newTask = '';
+            }
+        },
+        removeTask(index) {
+            this.tasks.splice(index, 1);
+        },
+        toggleTask(index) {
+            this.tasks[index].completed = !this.tasks[index].completed;
+        }
+    }
+}).mount('#app');
